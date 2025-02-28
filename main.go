1f41e52215e9e99ef25b41473acf9251eee90d9f
chelsea <git@cdhildit.ch>
2025-02-28 15:53:05 +0000

    Updates output to sort by price
    Increases the usability of the output by sorting the results and highlighting the best price found

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -38,10 +39,41 @@ func main() {
 	fmt.Println("Cheaper prices:")
 	fmt.Println("---------------")
 	for product, prices := range cheaperPrices {
-		for retailer, price := range prices {
-			fmt.Printf("%s is currently £%.2f at %s (-£%.2f)\n", product.Name, price, retailer.Name, product.BasePrice-price)
+		keys := sortRetailersByPrice(prices)
+
+		fmt.Printf("-- %s --\n", product.Name)
+		fmt.Printf("Base price: £%.2f\n", product.BasePrice)
+		fmt.Printf("Best price: £%.2f at %s %s\n", prices[keys[0]], keys[0].Name, getDiscountString(*product, prices[keys[0]]))
+
+		remaining := keys[1:]
+		if len(remaining) > 0 {
+			fmt.Println("Other prices:")
+			for _, key := range remaining {
+				fmt.Printf("- £%.2f at %s %s\n", prices[key], key.Name, getDiscountString(*product, prices[key]))
+			}
 		}
+
+		fmt.Println()
 	}
+}
+
+func sortRetailersByPrice(prices map[*Retailer]float64) []*Retailer {
+	keys := make([]*Retailer, 0, len(prices))
+	for retailer := range prices {
+		keys = append(keys, retailer)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return prices[keys[i]] < prices[keys[j]]
+	})
+
+	return keys
+}
+
+func getDiscountString(product Product, price float64) string {
+	discount := product.BasePrice - price
+	percentage := discount / product.BasePrice * 100
+	return fmt.Sprintf("(-£%.2f | %.2f%% off)", discount, percentage)
 }
 
 type Retailer struct {
